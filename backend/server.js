f995637e4e90ff7c6f1032ebd8a30f017a140568
Arnav <arnavkhandelwal@12.com>
2025-12-19 18:23:17 +0530

    how are you

--- backend/server.js
+++ backend/server.js
@@ -11,27 +11,48 @@ const BASH_PATH = `"C:/Program Files/Git/bin/bash.exe"`;
 
 // Create GitHub Repository
 app.post("/create-repo", (req, res) => {
-  exec(`${BASH_PATH} ${SCRIPT_PATH}/init_repo.sh`, (err, stdout, stderr) => {
-    if (err) return res.status(500).send(stderr);
+  const { repo } = req.body;
+
+  if (!repo) {
+    return res.status(400).send("Repository name required");
+  }
+
+  exec(`${BASH_PATH} ${SCRIPT_PATH}/init_repo.sh ${repo}`, (error, stdout, stderr) => {
+    if (error) return res.status(500).send(stderr || error.message);
     res.send(stdout);
   });
 });
+
 
 // Create Branch
 app.post("/create-branch", (req, res) => {
-  exec(`${BASH_PATH} ${SCRIPT_PATH}/branch_manager.sh`, (err, stdout, stderr) => {
-    if (err) return res.status(500).send(stderr);
-    res.send(stdout);
-  });
+  const { branch } = req.body;
+
+  if (!branch) {
+    return res.status(400).send("Branch name required");
+  }
+
+  exec(`${BASH_PATH} ${SCRIPT_PATH}/branch_manager.sh ${branch}`, 
+    (error, stdout, stderr) => {
+      if (error) return res.status(500).send(stderr || error.message);
+      res.send(stdout);
+    }
+  );
 });
+
 
 // Auto Commit
 app.post("/auto-commit", (req, res) => {
-  exec(`${BASH_PATH} ${SCRIPT_PATH}/auto_commit.sh`, (err, stdout, stderr) => {
-    if (err) return res.status(500).send(stderr);
-    res.send(stdout);
-  });
+  const { message } = req.body;
+
+  exec(`${BASH_PATH} ${SCRIPT_PATH}/auto_commit.sh "${message}"`, 
+    (error, stdout, stderr) => {
+      if (error) return res.status(500).send(stderr || error.message);
+      res.send(stdout);
+    }
+  );
 });
+
 
 // Sync Repository
 app.post("/sync", (req, res) => {
